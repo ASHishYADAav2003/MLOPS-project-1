84f60bee302e2755fe2d38f0b59b2665de817d71
ASHishYADav2003 <yadavashish2003060@gmail.com>
2026-03-30 19:45:22 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ashish Kumar",
+    author_email="yadavashish2003060@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
